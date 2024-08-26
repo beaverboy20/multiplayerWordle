a7a21895c319f3ba22ebd20289f24776cae1080c
beaverboy20 <79234660+beaverboy20@users.noreply.github.com>
2024-08-25 21:54:29 -0400

    push

---
+++ src/host.js
@@ -0,0 +1,11 @@
+function hostStart(){
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.rect(20, 20, 150, 100);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.stroke();
+}
+
--- src/main.js
+++
@@ -1,9 +0,0 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-ctx.beginPath();
-ctx.rect(20, 20, 150, 100);
-ctx.fillStyle = "red";
-ctx.fill();
-ctx.stroke();
-
