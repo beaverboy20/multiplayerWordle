97a7b283df643fb1589114d63b83bbb96408d1c1
beaverboy20 <79234660+beaverboy20@users.noreply.github.com>
2024-08-26 21:27:22 -0400

    change'em

--- src/host.js
+++ src/host.js
@@ -1,11 +1,37 @@
 function hostStart(){
-    const canvas = document.getElementById("canvas");
+
+    document.body.innerHTML = '';
+    const canvas = document.createElement("canvas");
+    canvas.width    = 800;
+    canvas.height   = 600;
+    canvas.style    = "border:1px solid #000000;";
+    const body = document.getElementsByTagName("body")[0];
+    body.appendChild(canvas);
     const ctx = canvas.getContext("2d");
 
+    ctx.font = "20px Arial Bold Italic";
+    ctx.font.fontStretch="ultra-condensed";
+    fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+        ctx.fillText("your adress:" + data.ip, 100, 60);
+    })
+    .catch(error => {
+        ctx.fillText("error" + error, 100, 50);
+        console.log("error:", error);
+    });
+
     ctx.beginPath();
-    ctx.rect(20, 20, 150, 100);
+    ctx.rect(100, 100, 150, 100);
     ctx.fillStyle = "red";
     ctx.fill();
     ctx.stroke();
+
+    
 }
+
+function draw(){
+
+}
+setInterval(draw, 10);
 
