3df640c2e0c7ea3a73f45bdc9be684bf1bbaf7dc
beaverboy20 <joshziegler76@gmail.com>
2024-09-09 10:51:56 -0400

    letters and backspace

--- src/host.js
+++ src/host.js
@@ -1,9 +1,55 @@
+var letterIndex = 0;
+var letters = [];
+var currentRowElement;
+var currentRow = "row1";
+function newRow(){
+    currentRowElement = document.getElementById(currentRow);
+    for (const element of currentRowElement.children){
+		letters.push(element);
+	}
+}
+newRow();
+
+
 function hostStart(){
     let theNameOfTheURL = window.locaton;
-    //window.location.replace(theNameOfTheURL.slice(0, theNameOfTheURL.length - 10) + "host.html");
     console.log("S");
     window.location.href = "host.html";
     
-    addEventListener("keypress", (
 
 }
+
+function keyPress(event){
+    var key;
+    if (window.event) {
+        key = event.keyCode;
+    } else if (event.which) {
+        key = event.which;
+    }
+    
+    letters[letterIndex].innerHTML = key;
+}
+
+
+
+document.addEventListener("keydown", function(event){
+	console.log(event.code[3]);
+	if (/[A-Z]/.test(event.code[3])){
+		if (letterIndex >= 0 && letterIndex < 5) {
+			letters[letterIndex].innerHTML = event.code[3];
+			letterIndex++;
+		}
+	}
+	if (event.code[3] == "k"){
+		if (letterIndex > 0) letterIndex--;
+		letters[letterIndex].innerHTML = "";
+	}
+	//event.code[3];
+	//letterIndex += 1;
+}); 
+
+
+
+
+
+
